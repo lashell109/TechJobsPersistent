dc7dffa80be25180d7bc531344f9a085b80147ba
La Shell Slaughter <lashellslaughter109@gmail.com>
2021-01-03 20:25:15 -0600

    Edited Json

--- TechJobsPersistent/ViewModels/AddJobViewModel.cs
+++ TechJobsPersistent/ViewModels/AddJobViewModel.cs
@@ -1,11 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TechJobsPersistent.Models;
 
 namespace TechJobsPersistent.ViewModels
 {
     public class AddJobViewModel
     {
+        public string Name { get; set; }
+        public int EmployerId { get; set; }
+        public string[] SelectedSkills { get; set; }
+
+        public List<SelectListItem> AllEmployers { get; set; }
+        public List<Skill> AllSkills { get; set; }
+
+        public AddJobViewModel() { }
+        public AddJobViewModel(List<Employer> employers, List<Skill> skills)
+        {
+            AllEmployers = new List<SelectListItem>();
+            foreach (Employer employer in employers)
+            {
+                AllEmployers.Add(new SelectListItem
+                {
+                    Text = employer.Name,
+                    Value = employer.Id.ToString()
+                });
+            }
+
+            AllSkills = skills;
+        }
     }
 }
